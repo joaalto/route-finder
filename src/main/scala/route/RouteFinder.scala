dab67c88395db420e447614e335ab034c5a68d65
Joonas Aalto <joaalto@iki.fi>
2014-03-31 00:41:57 +0300

    First draft of finding favourite route.

--- src/main/scala/route/RouteFinder.scala
+++ src/main/scala/route/RouteFinder.scala
@@ -4,12 +4,24 @@ import scala.io.Source
 
 class RouteFinder {
 
+  type RouteTree = List[List[Int]]
+
   def readRouteTree(fileName: String) = {
     val lines = Source.fromFile(fileName).getLines().toList
     val strList = lines.map(_.split(" ").toList)
-    strList.map((line: List[String]) => line.foreach(println(_)))
     strList.map((line: List[String]) => line.map(_.toInt))
   }
 
-  def findFavouriteRoute(routeTree: List[List[Int]]) = ""
+  def findFavouriteRoute(routeTree: RouteTree) = {
+    def addLikes(routeTree: RouteTree, column: Int, sum: Int): Int = {
+      if (routeTree.size == 1) sum
+      else {
+        val head = routeTree.head
+        val max = head(column).max(head(column + 1))
+        addLikes(routeTree.tail, head.indexOf(max, column), sum + max)
+      }
+    }
+    addLikes(routeTree.tail, 0, routeTree.head(0))
+  }
+
 }
