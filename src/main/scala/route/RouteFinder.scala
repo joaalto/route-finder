750dac08ec2c856a58d364178579b84f606618cf
Joonas Aalto <joaalto@iki.fi>
2014-03-31 23:39:50 +0300

    Return seed and sum from file.

--- src/main/scala/route/RouteFinder.scala
+++ src/main/scala/route/RouteFinder.scala
@@ -6,12 +6,19 @@ class RouteFinder {
 
   type RouteTree = List[List[Int]]
 
-  def readRouteTree(fileName: String): RouteTree = {
-    val lines = Source.fromFile(fileName).getLines.toList
-    lines.tail.map(_.split(" ").map(_.toInt).toList)
+  def seedAndSum(fileName: String): (Int, Int) = {
+    val tree = readRouteFile(fileName)
+    (tree._1, sumFavouriteRoute(tree._2))
   }
 
-  def findFavouriteRoute(routeTree: RouteTree) = {
+  def readRouteFile(fileName: String): (Int, RouteTree) = {
+    val lines = Source.fromFile(fileName).getLines.toList
+    val seed = lines.head.split(" ")(2).toInt
+    val tree = lines.tail.map(_.split(" ").map(_.toInt).toList)
+    (seed, tree)
+  }
+
+  def sumFavouriteRoute(routeTree: RouteTree) = {
     sumLikes(routeTree.tail, 0, routeTree.head(0))
   }
 
--- src/test/scala/route/RouteFinderSpec.scala
+++ src/test/scala/route/RouteFinderSpec.scala
@@ -7,17 +7,17 @@ class RouteFinderSpec extends FunSuite {
   val routeFinder = new RouteFinder()
 
   test("Tree 0 result should be 243") {
-    val routeTree = routeFinder.readRouteTree("src/test/scala/route/tree0.txt")
-    assert(routeFinder.findFavouriteRoute(routeTree) === 243)
+    assert(routeFinder.seedAndSum("src/test/scala/route/tree0.txt")
+      === (000, 243))
   }
 
   test("Tree 1 result should be 606") {
-    val routeTree = routeFinder.readRouteTree("src/test/scala/route/tree1.txt")
-    assert(routeFinder.findFavouriteRoute(routeTree) === 606)
+    assert(routeFinder.seedAndSum("src/test/scala/route/tree1.txt")
+      === (111, 606))
   }
 
   test("Tree 2 result should be 634") {
-    val routeTree = routeFinder.readRouteTree("src/test/scala/route/tree2.txt")
-    assert(routeFinder.findFavouriteRoute(routeTree) === 634)
+    assert(routeFinder.seedAndSum("src/test/scala/route/tree2.txt")
+      === (222, 634))
   }
 }
